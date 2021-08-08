2d0dccf17b26384158c4ec61a58a731f20327a0e
KMyroslav <kukhtarukm@gmail.com>
2021-08-08 18:38:46 +0300

    Finishes timer

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-const startBtn = document.querySelector('[data-action="start"]');
-const stopBtn = document.querySelector('[data-action="stop"]');
 const dayEl = document.querySelector('[data-value="days"]');
 const hoursEl = document.querySelector('[data-value="hours"]');
 const minsEl = document.querySelector('[data-value="mins"]');
@@ -10,24 +8,14 @@ class CountdownTimer {
     this.selector = selector;
     this.targetDate = targetDate;
     this.onTick = onTick;
-    this.timerId = null;
-    this.active = false;
+    this.start();
   }
 
   start() {
-    if (this.active) {
-      return;
-    }
-    this.active = true;
     this.timerId = setInterval(() => {
       const timeLeft = this.targetDate - Date.now();
       this.onTick(timeLeft);
     }, 1000);
-  }
-
-  stop() {
-    this.active = false;
-    clearInterval(this.timerId);
   }
 }
 
@@ -51,57 +39,3 @@ const timer = new CountdownTimer({
   targetDate: new Date('Jan 01, 2022'),
   onTick: updateTimer,
 });
-
-startBtn.addEventListener('click', timer.start.bind(timer));
-stopBtn.addEventListener('click', timer.stop.bind(timer));
-
-// const timer = {
-//   start() {
-//     if (active) {
-//       return;
-//     }
-//     active = true;
-//     timerId = setInterval(countdown, 1000);
-//     function countdown() {
-//       const startTime = Date.now();
-//       const timeLeft = targetDate - startTime;
-//       dayEl.textContent = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-//       hoursEl.textContent = pad(
-//         Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-//       );
-//       minsEl.textContent = pad(
-//         Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60)),
-//       );
-//       secsEl.textContent = pad(Math.floor((timeLeft % (1000 * 60)) / 1000));
-//     }
-//   },
-//   stop() {
-//     active = false;
-//     clearInterval(timerId);
-//   },
-// };
-
-// /*
-//  * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
-//  * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
-//  */
-// const days = Math.floor(time / (1000 * 60 * 60 * 24));
-
-// /*
-//  * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
-//  * остатка % и делим его на количество миллисекунд в одном часе
-//  * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
-//  */
-// const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
-// /*
-//  * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
-//  * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
-//  */
-// const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-
-// /*
-//  * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
-//  * миллисекунд в одной секунде (1000)
-//  */
-// const secs = Math.floor((time % (1000 * 60)) / 1000);
