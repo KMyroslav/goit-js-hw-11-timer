71bc05a8e42af02de4d6fce3af98a51fca872a09
KMyroslav <kukhtarukm@gmail.com>
2021-08-09 15:01:36 +0300

    Moves logic inside class

--- main.js
+++ main.js
@@ -1,41 +1,37 @@
-const dayEl = document.querySelector('[data-value="days"]');
-const hoursEl = document.querySelector('[data-value="hours"]');
-const minsEl = document.querySelector('[data-value="mins"]');
-const secsEl = document.querySelector('[data-value="secs"]');
-
 class CountdownTimer {
   constructor({ selector, targetDate, onTick }) {
     this.selector = selector;
     this.targetDate = targetDate;
-    this.onTick = onTick;
     this.start();
   }
 
   start() {
     this.timerId = setInterval(() => {
       const timeLeft = this.targetDate - Date.now();
-      this.onTick(timeLeft);
+      this.updateTimer(timeLeft);
     }, 1000);
   }
-}
-
-function pad(value) {
-  return String(value).padStart(2, '0');
-}
-
-function updateTimer(timeLeft) {
-  dayEl.textContent = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-  hoursEl.textContent = pad(
-    Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-  );
-  minsEl.textContent = pad(
-    Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60)),
-  );
-  secsEl.textContent = pad(Math.floor((timeLeft % (1000 * 60)) / 1000));
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+  updateTimer(timeLeft) {
+    const wrapper = document.querySelector(`${this.selector}`);
+    const dayEl = wrapper.querySelector('[data-value="days"]');
+    const hoursEl = wrapper.querySelector('[data-value="hours"]');
+    const minsEl = wrapper.querySelector('[data-value="mins"]');
+    const secsEl = wrapper.querySelector('[data-value="secs"]');
+    dayEl.textContent = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    hoursEl.textContent = this.pad(
+      Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+    minsEl.textContent = this.pad(
+      Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60)),
+    );
+    secsEl.textContent = this.pad(Math.floor((timeLeft % (1000 * 60)) / 1000));
+  }
 }
 
 const timer = new CountdownTimer({
   selector: '#timer-1',
-  targetDate: new Date('Jan 01, 2022'),
-  onTick: updateTimer,
+  targetDate: new Date('Dec 31, 2021'),
 });
